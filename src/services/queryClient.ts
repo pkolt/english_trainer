188a1e67e1492368c47b2774129f838ae28672a4
pkolt <pkoltyshev@gmail.com>
2024-10-19 09:56:13 +0400

    Fixed setup cache

--- src/services/queryClient.ts
+++ src/services/queryClient.ts
@@ -1,3 +1,14 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity, // No refetch on mount useQuery
+      gcTime: 1000 * 60 * 5, // Clear garbage collector time
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+    },
+  },
+});
