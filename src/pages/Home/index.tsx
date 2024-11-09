af963fc41d6bb60d4b2870b856d8cf700d9e44d2
pkolt <pkoltyshev@gmail.com>
2024-11-09 18:33:49 +0400

    Fixed home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,30 @@
 import DashboardPagesLayout from '@/layouts/DashboardPagesLayout';
-import { Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
+import LooksOneRoundedIcon from '@mui/icons-material/LooksOneRounded';
+import LooksTwoRoundedIcon from '@mui/icons-material/LooksTwoRounded';
+import Looks3RoundedIcon from '@mui/icons-material/Looks3Rounded';
 
 const Home = () => {
   return (
     <DashboardPagesLayout>
-      <Typography variant="h2" textAlign="center">
+      <Typography variant="h4" marginBottom={4}>
         Добро пожаловать в English Trainer!
       </Typography>
+
+      <Stack spacing={1}>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <LooksOneRoundedIcon fontSize="large" color="primary" />
+          <Typography variant="h5">Добавьте новые слова в словарь.</Typography>
+        </Stack>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <LooksTwoRoundedIcon fontSize="large" color="primary" />
+          <Typography variant="h5">Создайте списки слов для тренировки используя теги.</Typography>
+        </Stack>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Looks3RoundedIcon fontSize="large" color="primary" />
+          <Typography variant="h5">Тренируйте слова.</Typography>
+        </Stack>
+      </Stack>
     </DashboardPagesLayout>
   );
 };
