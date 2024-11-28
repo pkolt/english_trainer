fbe38e9414aae3477f6eab23df42008f1ad3b194
pkolt <pkoltyshev@gmail.com>
2024-11-28 21:02:38 +0400

    Fixed hooks

---
+++ src/hooks/useEffectNext.ts
@@ -0,0 +1,26 @@
+import { DependencyList, useEffect, useRef } from 'react';
+
+interface HookEffectNextProps {
+  effect: () => void;
+  condition?: () => boolean;
+  deps: DependencyList;
+}
+
+/** Skip first effect */
+export const useEffectNext = ({ effect, condition, deps }: HookEffectNextProps) => {
+  const refFlag = useRef(false);
+
+  useEffect(() => {
+    const isOk: boolean = condition ? condition() : true;
+
+    if (isOk) {
+      if (refFlag.current) {
+        effect();
+      } else {
+        refFlag.current = true;
+      }
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
+};
